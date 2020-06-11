e5c8c5f47b430ac5db726ddddcd28a40effca4ce
Kenneth Simpson <kenneth.mathis.simpson@gmail.com>
2020-06-11 16:07:20 -0500

    working on responsiveness for bottomnav, got successful default style

--- includes/bottomnav-surgis.php
+++ includes/bottomnav-surgis.php
@@ -1,64 +1,63 @@
 <footer>
 
-<div class="bottomNav">
+    <div class="bottomNav">
 
-    <div>
-        <h5>Navigation</h5>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About Us</a></li>
-            <li><a href="">Certifications</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">Our Team</a></li>
-            <li><a href="">Contact Us</a></li>
-            <li><a href="">Links</a></li>
-        </ul>
-    </div>
-    <div class="firmLicense">
-        <h5>License</h5>
-        <ul>
-            <li>Texas Board of Professional Engineers and Land Surveyors</li>
-            <li>Firm License #10193840</li>
-        </ul>
-    </div>
-    <div>
-        <h5>Contact Us</h5>
-        <ul>
-            <li>SurGIS of Texas Corporation</li>
-            <li>12274 Bandera Rd Ste 123</li>
-            <li>Helotes, TX 78023</li>
-            <!-- <br> -->
-            <li><a href="tel:2103675843">(210) 367-5843</a></li>
-            <li>Email: <a href="mailto:info@surgis-texas.com" target="_blank">info@surgis-texas.com</a></li>
-        </ul>
-        <div class="logosholder">
-            <a href="https://www.facebook.com/SurGIS/" target="_blank"><img src="/img/facebooklogo.png" alt="facebook link to SurGIS of Texas Corp"></a>
-            <a href="https://www.instagram.com/surgisoftexascorporation/?hl=en" target="_blank"><img src="/img/instagram.jpg" alt="instagram link to SurGIS of Texas Corp"></a>
+        <div id="sec1">
+            <h5>Navigation</h5>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Certifications</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Our Team</a></li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">Links</a></li>
+            </ul>
         </div>
-    </div>
-    <div>
-        <h5>Recommended Links</h5>
-        <ul>
-            <li><a href="https://comptroller.texas.gov/taxes/property-tax/county-directory/">Texas Appraisal Districts</a></li>
-            <li><a href="https://msc.fema.gov/portal/home">FEMA Flood Maps</a></li>
-            <li><a href="https://www.tsps.org/default.aspx">Texas Society of Professional Land Surveyors</a></li>
-            <li><a href="https://txls.texas.gov/">Texas Board of Prof. Engineers & Land Surveyors</a></li>
-            <li><a href="https://www.bexar.org/2950/Real-PropertyLand-Records">Bexar County Clerk</a></li>
-            <li><a href="https://www.sanantonio.gov/GIS">City of San Antonio GIS</a></li>
-            <li><a href="https://www.capcog.org/divisions/regional-planning-services#geographic">CAPCOG GIS</a></li>
-            <li><a href="">TxDOT ROW and Control</a></li>
-            <li><a href="https://www.nsps.us.com/page/ALTANSPSStandards">ALTA Land and Title Surveys</a></li>
-            <li><a href="https://westlat.com/">Westlat</a></li>
-            <li><a href="https://www.pobonline.com/">Point of Beginning Magazine</a></li>
-        </ul>
-    </div>
-
-
-        <div class="copyright-box">
-            <span id="copyright">&copy; 2020 SurGIS of Texas Corporation - All Rights Reserved </span>
-            <span>Texas Board of Professional Engineers and Land Surveyors Firm License #10193840</span>
+        <div id="sec2" class="firmLicense">
+            <h5>License</h5>
+            <ul>
+                <li>Texas Board of Professional Engineers and Land Surveyors</li>
+                <li>Firm License #10193840</li>
+            </ul>
         </div>
-</div>
+        <div id="sec3">
+            <h5>Contact Us</h5>
+            <ul>
+                <li>SurGIS of Texas Corporation</li>
+                <li>12274 Bandera Rd Ste 123</li>
+                <li>Helotes, TX 78023</li>
+                <li><a href="tel:2103675843">(210) 367-5843</a></li>
+                <li>Email: <a href="mailto:info@surgis-texas.com" target="_blank">info@surgis-texas.com</a></li>
+            </ul>
+            <div class="logosholder">
+                <a href="https://www.facebook.com/SurGIS/" target="_blank"><img src="/img/facebooklogo.png" alt="facebook link to SurGIS of Texas Corp"></a>
+                <a href="https://www.instagram.com/surgisoftexascorporation/?hl=en" target="_blank"><img src="/img/instagram.jpg" alt="instagram link to SurGIS of Texas Corp"></a>
+            </div>
+        </div>
+        <div id="sec4">
+            <h5>Recommended Links</h5>
+            <ul>
+                <li><a href="https://comptroller.texas.gov/taxes/property-tax/county-directory/">Texas Appraisal Districts</a></li>
+                <li><a href="https://msc.fema.gov/portal/home">FEMA Flood Maps</a></li>
+                <li><a href="https://www.tsps.org/default.aspx">Texas Society of Professional Land Surveyors</a></li>
+                <li><a href="https://txls.texas.gov/">Texas Board of Prof. Engineers & Land Surveyors</a></li>
+                <li><a href="https://www.bexar.org/2950/Real-PropertyLand-Records">Bexar County Clerk</a></li>
+                <li><a href="https://www.sanantonio.gov/GIS">City of San Antonio GIS</a></li>
+                <li><a href="https://www.capcog.org/divisions/regional-planning-services#geographic">CAPCOG GIS</a></li>
+                <li><a href="">TxDOT ROW and Control</a></li>
+                <li><a href="https://www.nsps.us.com/page/ALTANSPSStandards">ALTA Land and Title Surveys</a></li>
+                <li><a href="https://westlat.com/">Westlat</a></li>
+                <li><a href="https://www.pobonline.com/">Point of Beginning Magazine</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="copyright-box">
+        <span id="copyright">&copy; 2020 SurGIS of Texas Corporation - All Rights Reserved </span>
+        <!-- <span>Texas Board of Professional Engineers and Land Surveyors Firm License #10193840</span> -->
+    </div>
     
 
 </footer>
