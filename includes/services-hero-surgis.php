2bbac40d49495681adc93ddb02dbd922314811c9
Kenneth Simpson <kenneth.mathis.simpson@gmail.com>
2020-09-30 12:06:22 -0500

    services page bootstrap free

--- includes/services-hero-surgis.php
+++ includes/services-hero-surgis.php
@@ -6,7 +6,7 @@
         <div class="servicesCont">
             <h2 id="our-services">Our Services</h2>
             <div class="services">
-                <a href="#b-n-d-s" id="a-bnds"><img src="/img/hillCountryCrossingSurvey.jpeg" alt=""><span>Boundary & Subdivision Surveys</span></a>
+                <a href="#b-n-d-s" id="a-bnds"><img src="/img/hillCountryCrossingSurvey.jpeg" alt=""><span>Boundary & Subdivision</span></a>
                 <a href="#topo-s" id="a-topos"><img src="/img/topoSA.jpeg" alt=""><span>Topographic Surveys</span></a>
                 <a href="#constr-s" id="a-constrs"><img src="/img/uvalde-stake-1.jpeg" alt=""><span>Construction Staking</span></a>
                 <a href="#a-n-t-s" id="a-ants"><img src="/img/gALTA.jpeg" alt=""><span>ALTA Surveys</span></a>
