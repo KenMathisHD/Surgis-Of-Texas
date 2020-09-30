26d0502cf8801f76f2fb6bcdd63d7f9671da58ee
Kenneth Simpson <kenneth.mathis.simpson@gmail.com>
2020-09-30 18:45:10 -0500

    slight img path change to services hero section

--- includes/services-hero-surgis.php
+++ includes/services-hero-surgis.php
@@ -6,12 +6,12 @@
         <div class="servicesCont">
             <h2 id="our-services">Our Services</h2>
             <div class="services">
-                <a href="#b-n-d-s" id="a-bnds"><img src="/img/hillCountryCrossingSurvey.jpeg" alt=""><span>Boundary & Subdivision</span></a>
-                <a href="#topo-s" id="a-topos"><img src="/img/topoSA.jpeg" alt=""><span>Topographic Surveys</span></a>
-                <a href="#constr-s" id="a-constrs"><img src="/img/uvalde-stake-1.jpeg" alt=""><span>Construction Staking</span></a>
-                <a href="#a-n-t-s" id="a-ants"><img src="/img/gALTA.jpeg" alt=""><span>ALTA Surveys</span></a>
-                <a href="#t-n-l-s" id="a-tnls"><img src="/img/treeSurvey.jpeg" alt=""><span>Tree Location Surveys</span></a>
-                <a href="#fema-fl-s" id="a-femafls"><img src="/img/flood-cert.jpeg" alt=""><span>FEMA Flood Certificates</span></a>
+                <a href="#b-n-d-s" id="a-bnds"><img src="/img/600/hillCountryCrossingSurveyx600.jpeg" alt=""><span>Boundary & Subdivision</span></a>
+                <a href="#topo-s" id="a-topos"><img src="/img/600/topoSAx600.jpeg" alt=""><span>Topographic Surveys</span></a>
+                <a href="#constr-s" id="a-constrs"><img src="/img/600/uvalde-stake-1x600.jpeg" alt=""><span>Construction Staking</span></a>
+                <a href="#a-n-t-s" id="a-ants"><img src="/img/600/gALTAx600.jpeg" alt=""><span>ALTA Surveys</span></a>
+                <a href="#t-n-l-s" id="a-tnls"><img src="/img/600/treeSurveyx600.jpeg" alt=""><span>Tree Location Surveys</span></a>
+                <a href="#fema-fl-s" id="a-femafls"><img src="/img/600/flood-certx600.jpeg" alt=""><span>FEMA Flood Certificates</span></a>
             </div>
         </div>
 
